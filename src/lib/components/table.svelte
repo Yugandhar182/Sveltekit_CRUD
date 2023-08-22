2856ed7ef65432a63c3186fc3a419115200a5242
Yugandhar182 <yugandhar@recruitly.io>
2023-08-22 15:28:24 +0530

    /

--- src/lib/components/table.svelte
+++ src/lib/components/table.svelte
@@ -8,7 +8,7 @@
 export  let selectedCandidate = null;
   export let candidates = [];
 
-  // Modal visibility state
+  
   let isModalOpen = false;
  
 let isPopupOpen = false;
@@ -25,7 +25,7 @@ if (index !== -1) {
 
   // Make an API call to update the server
   const response = await fetch(`https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E`, {
-    method: 'POST', // Use PUT method to update existing data
+    method: 'POST', 
     headers: {
       'Content-Type': 'application/json'
     },
